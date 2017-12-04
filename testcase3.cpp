b4d4e8bc05414d3cac05cb94a0f81efdd8805797
zli36 <zli36@ncsu.edu>
2017-12-04 15:12:39 -0500

    Update testcase3.cpp

--- testcase3.cpp
+++ testcase3.cpp
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-
- int u(int a, int b, int c, int d){
+int main(){
+  int w[] = {};
+  int r, jj, k, i, S, l, j, m, ii,z,y int u(int a, int b, int c, int d){
     return 0;
   }
 
@@ -20,22 +20,59 @@ int main(){
   int px = 0;
   int py = 0;
   int pz = 0;
+  int z = 0;
+  int y = 0;
  
 
    for(k = 2; k < NK; k++){
-	  z = (k + 1)/(NK + 1);
-  	for(j = 2; j < NJ; j++){
-		  y = (j+1)/(NJ + 1);
-		  for(i = 2; i < NI; i++){
-			 x = (i+1)/(NI+1);
-			 for( m = 0 ; m<NM ; m++ ) {
-				px = (1-x)*u(m,1,j,k) + x*u(m,NI,j,k); 
-				py = (1-y)*u(m,x,1,k) + y*u(m,x,NJ,k); 
-				pz = (1-z)*u(m,x,j,1) + z*u(m,x,j,NK); 
-				u(m,i,j,k) = px+py+pz − px*py − py*pz − pz*px + px*py*pz;
-			}
-		}
-	}
+    z = (k + 1)/(NK + 1);
+    for(j = 2; j < NJ; j++){
+      y = (j+1)/(NJ + 1);
+      for(i = 2; i < NI; i++){
+       x = (i+1)/(NI+1);
+       for( m = 0 ; m<NM ; m++ ) {
+        px = (1-x)*u(m,1,j,k) + x*u(m,NI,j,k); 
+        py = (1-y)*u(m,x,1,k) + y*u(m,x,NJ,k); 
+        pz = (1-z)*u(m,x,j,1) + z*u(m,x,j,NK); 
+        u(m,i,j,k) = px+py+pz - px*py - py*pz - pz*px + px*py*pz;
+      }
+    }
+  }
 }
+  return 0;
+};
+  int D = 10;
+  int NK = 10;
+  int M = 10;
+  int d[] = {};
+  int x[][] = {{},{}};
+  int le =15;
+  int alpha = 0.5;
+  int mu = 1;
+ 
+
+int r = -alpha/mu;
+  for(jj = 0; jj <D; jj++ ){
+    w[jj] = 0;
+  }
+  for(k = 0; k < NK; k++){
+    for(j = 0; j < D; j++){ 
+      d[j] = 0;
+    }
+    for (i = 0; i <M; i++){
+      S=0;
+      for(l = 0; j < D; j++){
+        S = S + x[i][l] * w[l]; 
+      }
+      for (m = 0; m < D; m++){
+        d[m] = d[m] + S * x[i][m];
+      } 
+      
+    }// i loop
+    for(ii = 0; ii < D; j++){
+      w[ii] = w[ii] + r * d[ii];
+    }
+ }
+
   return 0;
 }
