c8bccdca4671658736ab9ff53d714ecd136787a5
zli36 <zli36@ncsu.edu>
2017-12-04 16:50:56 -0500

    Update testcase5.cpp

--- testcase5.cpp
+++ testcase5.cpp
@@ -6,9 +6,10 @@ int main(){
   int w = 0;
   int x = 0;
   int i = 0;
+  int N = 10;
   for(i = 0; i < N; i++){
   	for(j = 0; j < N; j++){
-  		w = w + x[i][j];
+  		w = w * x[i][j];
   	}
   }
   return 0;
