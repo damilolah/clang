91dd29f8010c2306513a111607b22e181cb40c68
zli36 <zli36@ncsu.edu>
2017-12-04 14:36:49 -0500

    Update testcase3.cpp

--- testcase3.cpp
+++ testcase3.cpp
@@ -25,14 +25,14 @@ int main(){
    for(k = 2; k < NK; k++){
 	  z = (k + 1)/(NK + 1);
   	for(j = 2; j < NJ; j++){
-		  y = (j + 1)/(NJ + 1);
+		  y = (j+1)/(NJ + 1);
 		  for(i = 2; i < NI; i++){
-			 x = (i + 1)/(NI + 1);
-			 for( m = 0 ; m < NM ; m++ ) {
-				px = (1 − x) ∗ u(m,1,j,k) + x ∗ u(m,NI,j,k); 
-				py = (1 − y) ∗ u(m,x,1,k) + y ∗ u(m,x,NJ,k); 
-				pz = (1 − z) ∗ u(m,x,j,1) + z ∗ u(m,x,j,NK); 
-				u(m,i,j,k) = px + py + pz − px ∗ py − py ∗ pz − pz ∗ px + px ∗ py ∗ pz;
+			 x = (i+1)/(NI+1);
+			 for( m = 0 ; m<NM ; m++ ) {
+				px = (1-x)*u(m,1,j,k) + x*u(m,NI,j,k); 
+				py = (1-y)*u(m,x,1,k) + y*u(m,x,NJ,k); 
+				pz = (1-z)*u(m,x,j,1) + z*u(m,x,j,NK); 
+				u(m,i,j,k) = px+py+pz − px*py − py*pz − pz*px + px*py*pz;
 			}
 		}
 	}
