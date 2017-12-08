0c767821d3b1351091db663f7239afaee6a1ed79
Ruth Okoilu <ruthokoilu@yahoo.com>
2017-12-08 02:51:25 -0500

    Added test case 7

---
+++ testcase7.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+  int w = 0;
+  int z = 0;
+  int x[10] = {};
+  int a = 1;
+  int b[10] = {};
+  int y[10][10]={{},{}};
+  int i = 0;
+  int N = 10;
+  int j ;
+  for(i = 0; i < N; i++){
+   		w = w + x[i];
+   		z = sin(x[i]+b[j]) + a 
+   		for(j = 0; j < N; j++){
+  		w = w + y[i][j];
+  	}
+  	
+  }
+  return 0;
+}
