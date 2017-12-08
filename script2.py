452e4f4c81f3028f0070e514500ca58aa1d53323
Ruth Okoilu <ruthokoilu@yahoo.com>
2017-12-08 06:36:10 -0500

    Create script2.py

---
+++ script2.py
@@ -0,0 +1,11 @@
+import os
+
+os.system('\cp LoopFind.cpp ~/llvm/llvm/tools/clang/tools/extra/loop-find/')
+os.system('ninja loop-find’)
+
+a = os.popen('ls all_tests/').readlines()
+command = './bin/loop-find all_tests/'
+
+for b in a:
+	cmd = command + b[0:-1]
+	os.system(cmd + ' >> result.txt')
