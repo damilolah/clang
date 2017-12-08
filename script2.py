27b40ff24fbf8a687ec8b57064e7dceb3f4e1bf9
Ruth Okoilu <ruthokoilu@yahoo.com>
2017-12-08 06:42:11 -0500

    Update script2.py

--- script2.py
+++ script2.py
@@ -1,7 +1,7 @@
 import os
 
 os.system('\cp LoopFind.cpp ~/llvm/llvm/tools/clang/tools/extra/loop-find/')
-os.system('ninja loop-find’)
+os.system('ninja loop-find')
 
 a = os.popen('ls all_tests/').readlines()
 command = './bin/loop-find all_tests/'
